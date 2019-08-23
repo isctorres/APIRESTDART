1b8ebc2495c65f851f1af62b9bac12ee68ad2d3d
isctorres <isctorres@MacBook-Pro-de-Office.local>
2019-08-23 11:57:44 -0500

    actualizacion api

--- lib/model/AreasCliente.dart
+++ lib/model/AreasCliente.dart
@@ -2,12 +2,14 @@ import 'package:firstapi/firstapi.dart';
 import 'package:firstapi/model/Areas.dart';
 import 'package:firstapi/model/Clientes.dart';
 
+
+
+
 class AreasCliente extends ManagedObject<tblAreasCliente> implements tblAreasCliente{}
 class tblAreasCliente{
   @primaryKey
   int idAreaCliente;
-
-  //int noEmpleados; 
+  int noEmpleados; 
 
   @Relate(#areacte)
   Areas area;
